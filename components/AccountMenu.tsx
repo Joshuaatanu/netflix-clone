2134428244ad8742d4d8aceebcf374f422e0be7e
Joshua Atanu <atanu.joshua@gmail.com>
2023-04-22 16:38:36 +0100

    exclude

---
+++ components/AccountMenu.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { signOut } from "next-auth/react";
+
+interface AccountMenuProps {
+  visible?: boolean;
+}
+
+export const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
+  if (!visible) {
+    return null;
+  }
+
+  return (
+    <div className="bg-black w-56 absolute top-14 right-0  py-5 flex-col border-2 border-gray-800 flex">
+        <div className="flex flex-col gap-3">
+            <div className="px-3 group/item flex flex-col  gap-3 items-center w-full ">
+                <img src="/images/default-blue.png" alt="" />
+            </div>
+        </div>
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
+import Navbar from "@/components/Navbar"
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -8,28 +8,21 @@ export async function getServerSideProps(context: NextPageContext) {
   if (!session) {
     return {
       redirect: {
-        destination : '/auth',
+        destination: "/auth",
         permanent: false,
       },
     };
   }
 
-return {
-  props : {}
-}
-
+  return {
+    props: {},
+  };
 }
 
 export default function Home() {
-  const {data:user} =useCurrentUser();
-
   return (
     <>
-      <h1 className="text-2xl  text-green-500">Netflix clone</h1>
-      <p className="text-white">Logged in as {user?.email} </p>
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
-        logout
-      </button>
+      <Navbar/>
     </>
   );
 }
