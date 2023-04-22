ba85224b48bd2217467f0369418ce9c4a816de0f
Joshua Atanu <atanu.joshua@gmail.com>
2023-04-22 14:14:21 +0100

    created the profile page

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home() {
   return (
     <>
       <h1 className="text-2xl  text-green-500">Netflix clone</h1>
-      <p className="text-white">Logged in as {user?.name} </p>
+      <p className="text-white">Logged in as {user?.email} </p>
       <button className="h-10 w-full bg-white" onClick={() => signOut()}>
         logout
       </button>
