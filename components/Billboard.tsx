c30db35f4ce6305387a8a66cf139057c99b1ffb9
Joshua Atanu <atanu.joshua@gmail.com>
2023-04-23 15:41:37 +0100

    finished modal

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -1,10 +1,16 @@
 import useBillboard from "@/hooks/useBillboard";
-import React from "react";
+import React, { useCallback } from "react";
 import { AiOutlineInfoCircle } from "react-icons/ai";
 import { PlayButton } from "./PlayButton";
+import useInfoModal from "@/hooks/useInfoModal";
 
 export const Billboard = () => {
   const { data } = useBillboard();
+  const { openModal } = useInfoModal();
+
+  const handleOpenModal = useCallback(() => {
+    openModal(data?.id);
+  }, [openModal, data?.id]);
   return (
     <div className=" realtive h-[56.25vw]">
       <video
@@ -52,6 +58,7 @@ export const Billboard = () => {
         <div className="flex flex-row items-center mt-3 md:mt-4  gap-3">
           <PlayButton movieId={data?.id} />
           <button
+          onClick={handleOpenModal} 
             className="
           bg-white
           text-white
